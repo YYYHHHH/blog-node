4a72d15bbcdc5b115f8caf081a93fd8567d4c32a
yanhan <1204676580@qq.com>
2018-04-12 19:26:44 +0800

    新增文章接口

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ var router = require('./router');
 // 导入MySQL模块
 var mysql = require('mysql');
 var dbConfig = require('./DBConfig');
-var userSQL = require('./usersql');
+var SQLForm = require('./SQLForm');
 // 使用DBConfig.js的配置信息创建一个MySQL连接池
 var pool = mysql.createPool( dbConfig.mysql );
 //设置跨域访问
@@ -17,6 +17,32 @@ app.all('*', function(req, res, next) {
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
+
+//发表文章
+app.use('/article/report',function (req,res,next) {
+    var reportSqlParams = [req.query.title,req.query.content,req.query.secret,req.query.create_time];
+    pool.getConnection(function (err, connection) {
+        connection.query(SQLForm.ArticleSQL.report,reportSqlParams, function (err, rows) {
+            if(err){
+                console.log('[SELECT ERROR] - ',err.message);
+                return;
+            }else{
+                res.send(JSON.stringify(
+                    {
+                        success:true,
+                        msg:'请求成功',
+                        data:rows
+                    }
+                ));
+            }
+            connection.release();//释放链接
+        });
+    });
+});
+
+
+
+
 app.use('/router',router);
 // 响应一个JSON数据
 app.get('/addUser',function (req, res, next) {
