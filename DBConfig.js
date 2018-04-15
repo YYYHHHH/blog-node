0b97f5f0e4e864e67b18d3ed137bba3942c5f912
YH <hzboye015@163.com>
2018-04-15 23:58:39 +0800

    支持post的参数获取； 提取数据库查询的公共方法

--- DBConfig.js
+++ DBConfig.js
@@ -3,7 +3,7 @@ module.exports = {
         host:'127.0.0.1',
         user:'root',
         password:'123456',
-        database:'expressdb',
+        database:'blog',
         port:3306
     }
 };
--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 var router = require('./router');
 // 导入MySQL模块
@@ -8,67 +12,81 @@ var dbConfig = require('./DBConfig');
 var SQLForm = require('./SQLForm');
 // 使用DBConfig.js的配置信息创建一个MySQL连接池
 var pool = mysql.createPool( dbConfig.mysql );
-//设置跨域访问
+
+/*//设置跨域访问   前端devServer设置了转发
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
-});
+});*/
+
+/**
+ * 数据库查询
+ * @param sql
+ * @returns {Promise<any>}
+ * @constructor
+ */
+
+const Model = (sql, params) => {
+    return new Promise((resolve, reject)=>{
+        pool.getConnection(function (err, connection) {
+            connection.query(sql,params, function (err, rows) {
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(rows);
+                }
+                connection.release();//释放链接
+            });
+        });
+    });
+};
 
 //发表文章
-app.use('/article/report',function (req,res,next) {
-    var reportSqlParams = [req.query.title,req.query.content,req.query.secret,req.query.create_time];
+app.post('/article/report',async function (req,res,next) {
+    let reportSqlParams = [req.body.title,req.body.content,req.body.secret,Date.now()];
+    try {
+        let results = await Model(SQLForm.ArticleSQL.report,reportSqlParams);
+        res.json({
+            success:true,
+            msg:'发表成功!',
+            data: results
+        });
+    } catch (err) {
+        next(err);
+        console.log('[SELECT ERROR] - ',err.message);
+    }
+});
+
+/*//发表文章
+app.post('/article/report',async function (req,res,next) {
+    let reportSqlParams = [req.body.title,req.body.content,req.body.secret,Date.now()];
     pool.getConnection(function (err, connection) {
         connection.query(SQLForm.ArticleSQL.report,reportSqlParams, function (err, rows) {
             if(err){
                 console.log('[SELECT ERROR] - ',err.message);
                 return;
             }else{
-                res.send(JSON.stringify(
+                res.json(
                     {
                         success:true,
-                        msg:'请求成功',
+                        msg:'发表成功!',
                         data:rows
                     }
-                ));
+                );
             }
             connection.release();//释放链接
         });
     });
-});
+});*/
 
 
 
 
 app.use('/router',router);
-// 响应一个JSON数据
-app.get('/addUser',function (req, res, next) {
-
-
-    var sql = 'SELECT * FROM websites';
-//查
-    pool.getConnection(function (err, connection) {
-        // Use the connection
-        connection.query(sql, function (err, rows) {
-            if(err){
-                console.log('[SELECT ERROR] - ',err.message);
-                return;
-            }else{
-                res.send(JSON.stringify(
-                    {
-                        success:true,
-                        msg:'请求成功',
-                        data:rows
-                    }
-                ));
-            }
-            connection.release();//释放链接
-        });
-    });
-});
 
 //中间件next()和next('route')的用法
 app.get('/nextRoute',function (req,res,next) {
